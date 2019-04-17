2b7b0586d9c98043868c0f691ce83f88084cd1e7
Tyler Landry <tyler.landry1996@outlook.com>
2019-04-17 22:28:06 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
-#write your code here
 
-def countdown
+
+def countdown(x)
+  counter = x 
+  while counter > 0 do
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(x)
+  counter = x 
+  while counter > 0 do
+    puts "#{counter} SECOND(S)!"
+    counter -= 1 
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end  
